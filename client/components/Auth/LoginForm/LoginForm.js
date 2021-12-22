4403644cce5f4e89263101f85b23edca748f716f
edtap14 <edgar.tapia@outlook.es>
2021-12-21 19:13:37 -0600

    [Update]

--- client/components/Auth/LoginForm/LoginForm.js
+++ client/components/Auth/LoginForm/LoginForm.js
@@ -1,11 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
+import { loginApi } from "../../../api/user";
 
 export default function LoginForm(props) {
-  const { showRegisterForm } = props;
+  const { showRegisterForm, onCloseModal } = props;
+  const [loading, setLoading] = useState(false);
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: async (formData) => {
+      setLoading(true);
+      const response = await loginApi(formData);
+      if (response?.jwt) {
+        console.log(response);
+        onCloseModal();
+      } else {
+        toast.error("El email o la contraseña es incorrecto");
+      }
+      setLoading(false);
+    }
+  });
+
   return (
-    <div>
-      <h1>Estamos en el formulario de Login</h1>
-      <button onClick={showRegisterForm}>Ir al registro</button>
-    </div>
+    <Form className="login-form" onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="identifier"
+        type="text"
+        placeholder="Correo Electronico"
+        onChange={formik.handleChange}
+        error={formik.errors.identifier}
+      />
+      <Form.Input
+        name="password"
+        type="password"
+        placeholder="Contraseña"
+        onChange={formik.handleChange}
+        error={formik.errors.password}
+      />
+      <div className="actions">
+        <Button type="button" basic onClick={showRegisterForm}>
+          Registrarse
+        </Button>
+        <div>
+          <Button type="submit" className="submit" loading={loading}>
+            Entrar
+          </Button>
+          <Button type="submit">¿Has olvidado la contraseña?</Button>
+        </div>
+      </div>
+    </Form>
   );
+}
+
+function initialValues() {
+  return {
+    identifier: "",
+    password: ""
+  };
+}
+
+function validationSchema() {
+  return {
+    identifier: Yup.string().email(true).required(true),
+    password: Yup.string().required(true)
+  };
 }
--- client/components/Auth/RegisterForm/RegisterForm.js
+++ client/components/Auth/RegisterForm/RegisterForm.js
@@ -2,15 +2,27 @@ import React, { useState } from "react";
 import { Form, Button, ButtonOr } from "semantic-ui-react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { registerApi } from "../../../api/user";
+import { toast } from "react-toastify";
 
 export default function RegisterForm(props) {
   const { showLoginForm } = props;
+  const [loading, setLoading] = useState(false);
 
   const formik = useFormik({
     initialValues: initialValues(),
     validationSchema: Yup.object(validationSchema()),
-    onSubmit: (formData) => {
-      console.log(formData);
+    onSubmit: async (formData) => {
+      setLoading(true);
+      const response = await registerApi(formData);
+      if (response?.jwt) {
+        toast.success("Registro de usuario correcto");
+        showLoginForm();
+      } else {
+        // console.log("Error al registrar el usuario intentelo más tarde");
+        toast.error("Error al registrar el usuario intentelo más tarde");
+      }
+      setLoading(false);
     }
   });
   return (
@@ -51,10 +63,10 @@ export default function RegisterForm(props) {
         error={formik.errors.password}
       />
       <div className="actions">
-        <Button type="button" basic>
+        <Button type="button" basic onClick={showLoginForm}>
           Iniciar Sesión
         </Button>
-        <Button type="submit" className="submit">
+        <Button type="submit" className="submit" loading={loading}>
           Registrar
         </Button>
       </div>
