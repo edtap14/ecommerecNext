18cc550613ef8e92bef860a777f8fca9e5892ecd
edtap14 <edgar.tapia@outlook.es>
2021-12-22 22:51:48 -0600

    [update] Se realizaron las validaciones de ingreso de usuario y una ruta privada

--- client/components/Header/Menu/Menu.js
+++ client/components/Header/Menu/Menu.js
@@ -1,14 +1,25 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Menu, Grid, Icon, Label } from "semantic-ui-react";
 import Link from "next/link";
 import BasicModal from "../../Modal/BasicModal";
 import Auth from "../../Auth";
+import useAuth from "../../../hooks/useAuth";
+import { getMeApi } from "../../../api/user";
 
 export default function MenuWeb() {
   const [showModal, setShowModal] = useState(false);
   const [tittleModal, setTittleModal] = useState("Iniciar Sesión");
+  const [user, setUser] = useState(undefined);
+  const { auth, logout } = useAuth();
   const onShowModal = () => setShowModal(true);
   const onCloseModal = () => setShowModal(false);
+
+  useEffect(() => {
+    (async () => {
+      const response = await getMeApi(logout);
+      setUser(response);
+    })();
+  }, [auth]);
 
   return (
     <div className="menu">
@@ -18,7 +29,13 @@ export default function MenuWeb() {
             <MenuPlatforms />
           </Grid.Column>
           <Grid.Column className="menu__right" width={10}>
-            <MenuOptions onShowModal={onShowModal} />
+            {user !== undefined && (
+              <MenuOptions
+                onShowModal={onShowModal}
+                user={user}
+                logout={logout}
+              />
+            )}
           </Grid.Column>
         </Grid>
       </Container>
@@ -51,13 +68,44 @@ function MenuPlatforms() {
 }
 
 function MenuOptions(props) {
-  const { onShowModal } = props;
+  const { onShowModal, user, logout } = props;
   return (
     <Menu>
-      <Menu.Item onClick={onShowModal}>
-        <Icon name="user outline" />
-        Mi cuenta
-      </Menu.Item>
+      {user ? (
+        <>
+          <Link href="/orders">
+            <Menu.Item as="a">
+              <Icon name="game" />
+              Mis pedidos
+            </Menu.Item>
+          </Link>
+          <Link href="/wishlist">
+            <Menu.Item as="a">
+              <Icon name="heart outline" />
+              Favoritos
+            </Menu.Item>
+          </Link>
+          <Link href="/account">
+            <Menu.Item as="a">
+              <Icon name="user outline" />
+              {user.name} {user.lastname}
+            </Menu.Item>
+          </Link>
+          <Link href="/cart">
+            <Menu.Item as="a" className="m-0">
+              <Icon name="cart" />
+            </Menu.Item>
+          </Link>
+          <Menu.Item onClick={logout}>
+            <Icon name="power of" className="m-0"></Icon>
+          </Menu.Item>
+        </>
+      ) : (
+        <Menu.Item onClick={onShowModal}>
+          <Icon name="user outline" />
+          Mi cuenta
+        </Menu.Item>
+      )}
     </Menu>
   );
 }
